1828fa8a60fc8d003974f375a340631a34c1cf46
Chris <chris@chris03.com>
2018-01-25 22:24:06 -0500

    Use full api

--- app/weather.js
+++ app/weather.js
@@ -1,88 +1,67 @@
 const Config = {
-    url: 'https://meteo.gc.ca/rss/city/qc-76_f.xml',
-    forecastsCount: 3
+    url: 'http://dd.weather.gc.ca/citypage_weather/xml/QC/s0000712_f.xml',
+    forecastsCount: 4
 };
 
 var request = require('request');
-var parseString = require('xml2js').parseString;
-
-function filterEntryByCategory(entry) {
-    var searchTerms = Array.prototype.slice.call(arguments, 1);
-    return entry.filter(i => {
-        return searchTerms.some(t => i.category.indexOf(t) >= 0);
-    });
-}
-
-function parseEntry(entry) {
-    return entry.map(i => {
-        return {
-            'title': i.title[0],
-            'category': i.category[0].$.term,
-            // 'summary': i.summary[0]['_']
-        };
-    });
-}
+var libxmljs = require("libxmljs");
 
 function getWeather() {
 
     return new Promise(function (resolve, reject) {
 
-        request(Config.url, function (error, response, body) {
+        request(Config.url, { encoding: 'binary' }, function (error, response, body) {
             if (error) {
                 reject(error);
             } else {
-                parseString(body, function (err, result) {
-                    var entries = parseEntry(result.feed.entry);
 
-                    var actual = filterEntryByCategory(entries, 'actuelles').map(i => {
-                        var pos1 = i.title.indexOf(':');
-                        var pos2 = i.title.indexOf(',');
-                        var temp = parseFloat(i.title.substring(pos2 + 1).trim().replace(',','.').replace('C','')).toFixed(0);
-                        return {
-                            'temp': temp,
-                            'desc': i.title.substring(pos1+1, pos2).trim()
-                        };
-                    })[0];
+                // var xmlString = iconv.decode(new Buffer(body, 'latin1'), 'utf8');
 
-                    var warnings = filterEntryByCategory(entries, 'avertissements').map(i => i.title)[0];
+                var xmlDoc = libxmljs.parseXml(body);
 
-                    var forecasts = filterEntryByCategory(entries, 'Prévisions').splice(0, Config.forecastsCount).map(i => {
-                        var pos1 = i.title.indexOf(':');
-                        var when = i.title.substring(0, pos1).trim();
-                        var what = i.title.substring(pos1 + 1).trim();
+                var actual = {
+                    'temp': parseFloat(xmlDoc.get('currentConditions/temperature').text()).toFixed(0),
+                    'desc': xmlDoc.get('currentConditions/condition').text()
+                };
 
-                        when = when.replace(' et cette ','/')
-                                .replace('r et n','r/n');
-                        what = what.replace('moins ','-')
-                                   .replace('Minimum','Min.')
-                                   .replace('Maximum','Max.')
-                                   .replace('Possibilité','Possib.')
-                                   .replace('d\'averses de','de')
-                                   .replace('alternance','alt.')
-                                   .replace(' près de','')
-                                   .replace('pour atteindre','à')
-                                   .replace('zéro', '0')
-                                   .replace('à la baisse', "↓")
-                                   .replace('à la hausse', "↑")
-                                   .replace('Températures','Temp.')
-                                   .replace(' ou ','/')
-                                   .replace('au cours de', 'durant')
-                                   .replace('pluie ou de neige','pluie/neige')
-                                   .replace(' intermittente','');
+                var warnings = '';//filterEntryByCategory(entries, 'avertissements').map(i => i.title)[0];
 
-                        return {
-                            'when': when,
-                            'what': what
-                        };
-                    });
+                var forecasts = xmlDoc.find('//forecast').splice(0, Config.forecastsCount).map(i => {
+                    var when = i.get('period').text();
+                    var what = i.get('abbreviatedForecast/textSummary').text();
+                    var temp = i.get('temperatures/temperature').text();
 
-                    // Return data
-                    resolve({
-                        'actual': actual,
-                        'forecasts': forecasts,
-                        'warnings': warnings
-                    });
+                    when = when.replace(' et cette ', '/')
+                        .replace('r et n', 'r/n');
+                    what = what.replace('moins ', '-')
+                        .replace('soleil et de nuages', 'soleil/nuages')
+                        .replace('Minimum', 'Min.')
+                        .replace('Maximum', 'Max.')
+                        .replace('Possibilité', 'Possib.')
+                        .replace('d\'averses de', 'de')
+                        .replace('alternance', 'alt.')
+                        .replace(' près de', '')
+                        .replace('pour atteindre', 'à')
+                        .replace('zéro', '0')
+                        .replace('à la baisse', "↓")
+                        .replace('à la hausse', "↑")
+                        .replace('Températures', 'Temp.')
+                        .replace(' ou ', '/')
+                        .replace('au cours de', 'durant')
+                        .replace('pluie ou de neige', 'pluie/neige')
+                        .replace(' intermittente', '');
 
+                    return {
+                        'when': when.charAt(0).toUpperCase() + when.slice(1),
+                        'what': what + ' ' + temp,
+                    };
+                });
+
+                // Return data
+                resolve({
+                    'actual': actual,
+                    'forecasts': forecasts,
+                    'warnings': warnings
                 });
             }
         });

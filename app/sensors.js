b64ed2398bb12ba4f53a16d7c5def680abc3eb06
Chris <chris@chris03.com>
2020-07-04 13:53:18 -0400

    Add sensor name & up flag

--- app/sensors.js
+++ app/sensors.js
@@ -10,6 +10,22 @@ function getFloat(sensors, id, property, precision) {
     return sensor == undefined || sensor[property] == undefined ? '' : parseFloat(sensor[property]).toFixed(precision || 0);
 }
 
+function isUp(sensors, id) {
+    var result = false;
+    var sensor = sensors.find(s => s.idx == id);
+
+    if (sensor != undefined && sensor['LastUpdate'] != undefined) {
+        var now = new Date();
+        var lastUpdate = new Date(sensor['LastUpdate']);
+
+        // Total minutes difference
+        var diff = Math.abs(now - lastUpdate);
+        result = Math.floor((diff / 1000) / 60) <= 10;
+    }
+
+    return result;
+}
+
 function formatNegativeNumber(val) {
     var result = val.toString();
 
@@ -41,30 +57,42 @@ function getSensors(serverUrl) {
                         lux: getValue(sensors, 47, 'Data'),
                         energy: getValue(sensors, 52, 'Data'),
                         pump: getValue(sensors, 49, 'Data'),
+                        up: isUp(sensors, 43)
                     },
-                    outside: {
-                        temp: getValue(sensors, 45, 'Temp'),
-                        hum: getValue(sensors, 45, 'Humidity'),
-                    },
-                    air: {
-                        temp: getValue(sensors, 51, 'Temp'),
-                        hum: getValue(sensors, 51, 'Humidity'),
-                    },
-                    arduino1: {
-                        temp: getValue(sensors, 38, 'Temp'),
-                        hum: getValue(sensors, 38, 'Humidity'),
-                        volt: getValue(sensors, 44, 'Voltage', 2),
-                    },
-                    arduino2: {
-                        temp: getValue(sensors, 39, 'Temp'),
-                        hum: getValue(sensors, 39, 'Humidity'),
-                        volt: getValue(sensors, 42, 'Voltage', 2),
-                    },
-                    garage: {
-                        temp: getValue(sensors, 59, 'Temp'),
-                        hum: getValue(sensors, 59, 'Humidity'),
-                        volt: getValue(sensors, 60, 'Voltage', 2),
-                    },
+                    sensors: [
+                        {
+                            name: getValue(sensors, 45, 'Name'),
+                            temp: getValue(sensors, 45, 'Temp'),
+                            hum: getValue(sensors, 45, 'Humidity'),
+                            up: isUp(sensors, 45)
+                        },
+                        {
+                            name: getValue(sensors, 51, 'Name'),
+                            temp: getValue(sensors, 51, 'Temp'),
+                            hum: getValue(sensors, 51, 'Humidity'),
+                            up: isUp(sensors, 51)
+                        },
+                        {
+                            name: getValue(sensors, 38, 'Name'),
+                            temp: getValue(sensors, 38, 'Temp'),
+                            hum: getValue(sensors, 38, 'Humidity'),
+                            volt: getValue(sensors, 44, 'Voltage', 2),
+                            up: isUp(sensors, 38)
+                        },
+                        {
+                            name: getValue(sensors, 39, 'Name'),
+                            temp: getValue(sensors, 39, 'Temp'),
+                            hum: getValue(sensors, 39, 'Humidity'),
+                            volt: getValue(sensors, 42, 'Voltage', 2),
+                            up: isUp(sensors, 39)
+                        },
+                        {
+                            name: getValue(sensors, 59, 'Name'),
+                            temp: getValue(sensors, 59, 'Temp'),
+                            hum: getValue(sensors, 59, 'Humidity'),
+                            volt: getValue(sensors, 60, 'Voltage', 2),
+                            up: isUp(sensors, 59)
+                        }]
                 });
             }
         });
